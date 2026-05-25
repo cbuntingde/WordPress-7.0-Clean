314c12afcd1c7cd01549b80fb0bfdeae0ab8c1c6
cbuntingde <cbuntingde@gmail.com>
2026-05-24 20:20:09 -0400

    Untrack dev files - keep local-only
    Move to .git/info/exclude:
    - Docker (docker-compose, nginx/, php/)
    - Composer + vendor
    - Testing (tests/, sql/, test*.txt)
    - Static analysis (phpcs, phpstan, psalm configs)
    - Docs (license, readme, FUNDING)
    
    Updated .gitignore accordingly.

--- php/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-chown -R www-data:www-data /var/www/html/wp-content/
-exec docker-php-entrypoint php-fpm
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: text/plain");
-echo "URL: http://10.0.0.28:8090\n";
-$mysqli = new mysqli("wp_mariadb", "root", "root", "wordpress");
-echo "DB: " . ($mysqli ? "OK" : "FAIL") . "\n";
-
